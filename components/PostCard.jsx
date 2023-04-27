075ca99862f2359d5328fe8de9f11661d4aca94d
jamesscr <allanjeanjacques@hotmail.com>
2023-04-27 05:17:22 -0400

    dev mobile

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const PostCard = ({ post }) => {
   // console.log(post)
   return (
-    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
+    <div className="mb-8 w-full rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
       <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
         <img
           src={post.featuredImage.url}
